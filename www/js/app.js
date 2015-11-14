fecf14d7f7d0c64057de2397ee18ca81e3260d40
Fernando Contreras <fercreek@gmail.com>
2015-11-14 05:27:52 -0600

    Analytics and users

--- www/js/app.js
+++ www/js/app.js
@@ -1,8 +1,9 @@
 (function(){
-  var app = angular.module('starter', ['ionic','ionic.service.core', 'starter.controllers', 'starter.services']);
+  var app = angular.module('starter', ['ionic','ionic.service.core', 'ionic.service.analytics','starter.controllers', 'starter.services']);
 
-  app.run(function($ionicPlatform) {
+  app.run(function($ionicPlatform, $ionicAnalytics) {
     $ionicPlatform.ready(function() {
+      $ionicAnalytics.register();
       if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
         cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
         cordova.plugins.Keyboard.disableScroll(true);
@@ -19,6 +20,23 @@
       push.register(function(token) {
         console.log("Device token:",token.token);
       });
+
+      Ionic.io();
+
+      // this will give you a fresh user or the previously saved 'current user'
+      var user = Ionic.User.current();
+
+      // if the user doesn't have an id, you'll need to give it one.
+      if (!user.id) {
+        user.id = Ionic.User.anonymousId();
+        // user.id = 'your-custom-user-id';
+        console.log('!user',user.id );
+      }
+      console.log('userr',user.id );
+
+      //persist the user
+      user.save();
+
     });
   });
 
