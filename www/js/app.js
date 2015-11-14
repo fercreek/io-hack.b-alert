b54ea91cbd3af2efa2ffc4381af33745d6172891
Fernando Contreras <fercreek@gmail.com>
2015-11-14 05:36:11 -0600

    deploy added

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,9 @@
 
   app.run(function($ionicPlatform, $ionicAnalytics) {
     $ionicPlatform.ready(function() {
+
       $ionicAnalytics.register();
+      
       if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
         cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
         cordova.plugins.Keyboard.disableScroll(true);
